2daf04be83b259c5a8038f6ce2754e446754c768
Arda Coşkun <m.ardacoskun@gmail.com>
2022-06-30 12:57:11 +0300

    redux characterSlice created

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Home = () => {
+  const data = useSelector((state) => state.characters);
+
   return <div>Home</div>;
 };
 
