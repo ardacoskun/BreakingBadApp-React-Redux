bbd61b93b9fff767f31cd26f502f628a7d84e3cc
Arda Coşkun <m.ardacoskun@gmail.com>
2022-06-30 13:03:10 +0300

    redux thunk middleware defined to fetch datas async and getCharacters function created

--- src/store/characterSlice.js
+++ src/store/characterSlice.js
@@ -1,4 +1,13 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchCharacters = createAsyncThunk(
+  "characters/getCharacters",
+  async () => {
+    const response = await axios(`${process.env.REACT_APP_API_BASE_URL}`);
+    return await response.data;
+  }
+);
 
 export const characterSlice = createSlice({
   name: "character",
