244fa4cecf33037ac11a92fe155b608110fcd84c
Arda Coşkun <m.ardacoskun@gmail.com>
2022-07-01 18:37:26 +0300

    quoteDetail page find fuction update

--- src/pages/QuotesDetail.js
+++ src/pages/QuotesDetail.js
@@ -6,9 +6,9 @@ import { quotesSelector } from "../store/quotesSlice";
 const QuotesDetail = () => {
   const { quoteId } = useParams();
 
-  const items = useSelector(quotesSelector);
-
-  const quote = items.find((item) => item.quote_id === Number(quoteId));
+  const quote = useSelector((state) =>
+    state.quotes.items.find((item) => item.quote_id === Number(quoteId))
+  );
 
   return (
     <div className="quoteDetail">
