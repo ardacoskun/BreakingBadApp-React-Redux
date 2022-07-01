0b6b1b8c470ead34c7a98104b935e51e814ae45f
Arda Coşkun <m.ardacoskun@gmail.com>
2022-07-01 18:36:07 +0300

    quote detail page created

--- src/pages/QuotesDetail.js
+++ src/pages/QuotesDetail.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { quotesSelector } from "../store/quotesSlice";
 
 const QuotesDetail = () => {
-  return <div>QuotesDetail</div>;
+  const { quoteId } = useParams();
+
+  const items = useSelector(quotesSelector);
+
+  const quote = items.find((item) => item.quote_id === Number(quoteId));
+
+  return (
+    <div className="quoteDetail">
+      <h1>Quote Detail </h1>
+      <pre>{JSON.stringify(quote, null, 2)}</pre>
+    </div>
+  );
 };
 
 export default QuotesDetail;
