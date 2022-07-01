e1ce5561616c80e87779a170742bdccc45b66b9f
Arda Coşkun <m.ardacoskun@gmail.com>
2022-07-01 14:14:20 +0300

    character detail page designed

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/character/:char_id" element={<Detail />} />
+      <Route path="/character/:characterId" element={<Detail />} />
     </Routes>
   );
 };
--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,7 +1,37 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Loading } from "../components";
 
 const Detail = () => {
-  return <div>Detail</div>;
+  const [char, setChar] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const { characterId } = useParams();
+
+  useEffect(() => {
+    axios(`${process.env.REACT_APP_API_BASE_URL}/characters/${characterId}`)
+      .then((res) => res.data)
+      .then((data) => setChar(data[0]))
+      .finally(() => setLoading(false));
+  }, [characterId]);
+
+  return (
+    <div>
+      {loading && <Loading />}
+      {char && (
+        <div>
+          <h1>{char.name}</h1>
+          <img src={char.img} alt={char.name} style={{ width: "50%" }} />
+          <h3>Birthday: {char.birthday}</h3>
+          {char.occupation.map((item, index) => (
+            <div key={index}>{item}</div>
+          ))}
+          <h3>Status: {char.status}</h3>
+          <h3>Portrayed: {char.portrayed}</h3>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Detail;
